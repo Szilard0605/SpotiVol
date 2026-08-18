edfe0bf8b5620dc237de06b4557407293aa2df81
Szilárd Kátai <62345565+Szilard0605@users.noreply.github.com>
2026-08-18 19:22:39 +0200

    cleanup

--- scripts/dl_premake5.py
+++ scripts/dl_premake5.py
@@ -10,9 +10,13 @@ if premake_exe.is_file():
     print("premake5.exe is present.")
 else:
     print("Couldn't find premake5.exe, downloading it...")
+    dl_dir = Path("../premake5/")
+
+    if not dl_dir.exists():
+        os.makedirs(dl_dir)
 
     url = "https://github.com/premake/premake-core/releases/download/v5.0.0-beta8/premake-5.0.0-beta8-windows.zip"
-    zip_path = '../premake5/premake-5.0.0-beta8-windows.zip'
+    zip_path = str(dl_dir) + "premake-5.0.0-beta8-windows.zip"
     urllib.request.urlretrieve(url, zip_path)
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall("../premake5/")
